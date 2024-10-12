90c8181169d6a34916443fb6124797af3be9b032
dmitriizajcev82 <80349728+dmitriizajcev82@users.noreply.github.com>
2024-10-12 23:07:30 +0300

    Lesson26

--- Lesson26/client.sh
+++ Lesson26/client.sh
@@ -2,9 +2,9 @@
 sudo su
 
 ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-cp /tmp/files/borg-backup.sh /etc/
-cp /tmp/files/borg-backup.timer /etc/systemd/system
-cp /tmp/files/borg-backup.service /etc/systemd/system
+cp borg-backup.sh /etc/
+cp borg-backup.timer /etc/systemd/system
+cp borg-backup.service /etc/systemd/system
 chmod +x /etc/borg-backup.sh
 apt install borgbackup -y
 useradd -m borg
